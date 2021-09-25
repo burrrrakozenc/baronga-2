ec2f23ac0f438ea4a3448e86ee5637a02a571ff3
burrrrakozenc <brk.ozenc@gmail.com>
2021-09-25 15:17:39 +0300

    discount query updated

--- src/components/featured-products/featured-products-2.tsx
+++ src/components/featured-products/featured-products-2.tsx
@@ -5,42 +5,44 @@ import ProductGrid from '../product-grid/product-grid';
 const featuredStaticQuery = graphql`
 	query {
 		allShopifyProduct(filter: {variants: {elemMatch: {compareAtPriceV2: {amount: {ne: null}}}}}, limit: 10) {
-    nodes {
-      id
-      title
-      handle
-      createdAt
-      shopifyId
-      availableForSale
-      variants {
-        id
-        price
-        compareAtPrice
-        compareAtPriceV2 {
-          amount
-          currencyCode
-        }
-        priceV2 {
-          amount
-          currencyCode
-        }
-        shopifyId
-        availableForSale
-      }
-      images {
-        id
-        originalSrc
-        localFile {
-          childImageSharp {
-            fluid {
-				...GatsbyImageSharpFluid_withWebp_tracedSVG
+      edges {
+        node {
+          id
+          title
+          handle
+          createdAt
+          shopifyId
+          availableForSale
+          variants {
+            id
+            price
+            compareAtPrice
+            compareAtPriceV2 {
+              amount
+              currencyCode
+            }
+            priceV2 {
+              amount
+              currencyCode
+            }
+            shopifyId
+            availableForSale
+          }
+          images {
+            id
+            originalSrc
+            localFile {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                }
+              }
             }
           }
         }
       }
     }
   }
-	}
 `;
 
 const FeaturedProducts2: React.FC<{ withLink?: boolean }> = ({
